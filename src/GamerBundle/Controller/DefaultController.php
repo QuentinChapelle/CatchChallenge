40ca2b2b5082261638c82647b07e8e1f7474aa54
MathieuM1 <mat.mercier45@gmail.com>
2016-12-15 23:00:32 +0100

    update dashboard

--- src/GamerBundle/Controller/DefaultController.php
+++ src/GamerBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace GamerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use GamerBundle\Entity\Partie;
+use GamerBundle\Entity\Image;
 
 class DefaultController extends Controller
 {
@@ -15,11 +17,4 @@ class DefaultController extends Controller
         return $this->render('GamerBundle:Default:index.html.twig');
     }
 
-    /**
-     * @Route("/dashboard")
-     */
-    public function dashBoardMeneurAction()
-    {
-        return $this->render('@Gamer/Default/dashBoardMeneur.html.twig');
-    }
 }
